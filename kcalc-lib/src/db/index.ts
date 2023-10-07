56eb522fa5428dbf63bbc7e6c6fcbc276ba570f4
Pontus Östlund <ponost@roxen.com>
2023-10-07 11:39:30 +0200

    [kcalc-lib]: Added some types, typeguards and db helpers

--- kcalc-lib/src/db/index.ts
+++ kcalc-lib/src/db/index.ts
@@ -7,6 +7,7 @@ import {
   Nutrient,
   NutritionValue,
 } from './entities/index.js'
+import type { Maybe } from '../type-types.js'
 
 export { Food, FoodSource, Group, Nutrient, NutritionValue }
 
@@ -14,8 +15,26 @@ export type InitDbOptions = {
   path: string
 }
 
+let db_: Maybe<DataSource>
+
+export function db(): DataSource {
+  if (!db_) {
+    throw new Error(`You must call "init()" first to initialize the DB object`)
+  }
+
+  if (!db_.isInitialized) {
+    throw new Error(`You must await the DB initialization before calling db()`)
+  }
+
+  return db_
+}
+
 export async function init(options: InitDbOptions): Promise<DataSource> {
-  const db = new DataSource({
+  if (db_) {
+    return db()
+  }
+
+  db_ = new DataSource({
     type: 'sqlite',
     database: options.path,
     synchronize: true,
@@ -26,5 +45,5 @@ export async function init(options: InitDbOptions): Promise<DataSource> {
     subscribers: [],
   })
 
-  return db.initialize()
+  return db_.initialize()
 }
--- kcalc-lib/src/type-types.ts
+++ kcalc-lib/src/type-types.ts
@@ -5,4 +5,24 @@ export type MaybeFalsy<T> = T | null | undefined | 0 | false
 export type AnyFunction = (...args: any[]) => any
 export type VoidFunction = () => void
 export type PlainObject<T = any> = { [key: string | symbol | number]: T }
-export type Class<T = any> = new (...args: any[]) => T
+export type Class<T> = new (...args: any[]) => T
+export type Callable = () => any
+
+export type DecoratorFn = <T>(
+  _target: T,
+  _propertyKey: string,
+  descriptor: PropertyDescriptor
+) => void
+
+// prettier-ignore
+export type TypeOf<T> =
+  T extends string ? string :
+  T extends number ? number :
+  T extends boolean ? boolean :
+  T
+
+export type DeepPartial<T> = T extends AnyFunction
+  ? T
+  : T extends object
+  ? { [P in keyof T]?: DeepPartial<T[P]> }
+  : T
